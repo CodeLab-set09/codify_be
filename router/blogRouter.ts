624eac5e7d93f2d733e5b92664469587fb19f6d7
RashCodes09 <ayinkerasheedat500@gmail.com>
2024-09-27 11:11:12 +0100

    rasheedat

--- router/blogRouter.ts
+++ router/blogRouter.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
-import { createblog, readAllblog,readoneblog } from "../controller/blogcontroller";
+import {
+  createblog,
+  readAllblog,
+  readOneBlog,
+} from "../controller/blogcontroller";
 
 const router: Router = Router();
 
 router.route("/create-blog").post(createblog);
 
 router.route("/get-all-blog").get(readAllblog);
-router.route("/get-one-blog").get(readoneblog);
+router.route("/:blogID/get-one-blog").get(readOneBlog);
 
 export default router;
